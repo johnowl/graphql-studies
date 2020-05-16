37b26f15c992b9d855919115598b336e9f2dcdae
João Paulo <johnowl@gmail.com>
2020-05-16 14:47:01 -0300

    Prisma mutations.

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -5,6 +5,24 @@ const prisma = new Prisma({
     endpoint: 'http://localhost:4466'
 })
 
-prisma.query.users(null, '{ id name }').then((data) => {
+// prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2))
+// })
+
+prisma.mutation.createPost({
+    data: {
+        title: "GraphQL",
+        body: "Post body.",
+        published: true,
+        author: {
+            connect: {
+                id: "cka9wr0cx01ii0851h5reml99"
+            }
+        }
+    }
+}, '{ id title body published }').then((data) => {
     console.log(data)
+    return prisma.query.users(null, '{id name posts { id title } }')
+}).then((data) => {
+    console.log(JSON.stringify(data, undefined, 2))
 })
